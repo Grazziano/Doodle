68b997264ffa35ee7c17412cc3f5b1a2ccfd95df
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-08-05 20:34:49 -0300

    Creating our search page structure

--- search.php
+++ search.php
@@ -1,2 +1,35 @@
-<?php
-echo $_GET['term'];
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Doodle</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+            <div class="headerContent">
+                <div class="logoContainer">
+                    <a href="index.php">
+                        <img src="assets/images/doodleLogo.png" alt="">
+                    </a>
+                </div>
+
+                <div class="searchContainer">
+                    <form action="search.php" method="GET">
+                        <div class="searchBarContainer">
+                            <input class="searchBox" type="text" name="term">
+                            <button>Search</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
