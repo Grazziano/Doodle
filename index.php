129b9c3dafe9bc53f73abc46700c2a6e3e0dbaff
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-08-05 15:52:18 -0300

    Creating our index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Doodle</title>
+</head>
+
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="">
+        </div>
+
+        <div class="searchContainer">
+            <input class="searchBox" type="text" name="term">
+            <input class="searchButton" type="submit" value="Search">
+        </div>
+    </div>
+</body>
+
+</html>
