c9caa48ac1e35357f0c703a28af889bb5cef875c
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-08-08 16:00:07 -0300

    Converting relative links to absolute links - Part 2

--- crawl.php
+++ crawl.php
@@ -10,6 +10,12 @@ function createLink($src, $url)
         $src = parse_url($url)["scheme"] . ":" . $src;
     } else if (substr($src, 0, 1) == "/") {
         $src = $scheme . "://" . $host . $src;
+    } else if (substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src, 1);
+    } else if (substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    } else if (substr($src, 0, 5) != "https" && substr($src, 0, 4) != "http") {
+        $src = $scheme . "://" . $host . "/" . $src;
     }
     return $src;
 }
