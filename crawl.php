5513d072959f6ebbb41d163ea6016d990f3b3c80
Grazziano Fagundes <grazzianofagundes@gmail.com>
2021-08-08 23:15:50 -0300

    Crawling images

--- crawl.php
+++ crawl.php
@@ -4,6 +4,7 @@ include("classes/DomDocumentParser.php");
 
 $alreadyCrawled = array();
 $crawling = array();
+$alreadyFoundImages = array();
 
 function linkExists($url)
 {
@@ -89,6 +90,24 @@ function getDetails($url)
         echo "SUCCESS: $url<br>";
     } else {
         echo "ERROR: Failed to insert $url<br>";
+    }
+
+    $imageArray = $parser->getImages();
+    foreach ($imageArray as $image) {
+        $src = $image->getAttribute("src");
+        $alt = $image->getAttribute("alt");
+        $title = $image->getAttribute("title");
+
+        if (!$title && !$alt) {
+            continue;
+        }
+
+        $src = createLink($src, $url);
+
+        if (!in_array($src, $alreadyFoundImages)) {
+            $alreadyFoundImages[] = $src;
+            //insert the image
+        }
     }
 }
 
