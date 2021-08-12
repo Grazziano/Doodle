3795759b83141c1a9ae327de7b5ecc976568f9b0
Grazziano Fagundes <grazzianofagundes@gmail.com>
2021-08-11 23:35:06 -0300

    Showing the first and last images

--- search.php
+++ search.php
@@ -68,6 +68,18 @@ $page = isset($_GET["page"]) ? $_GET["page"] : 1;
             ?>
         </div>
 
+        <div class="paginationContainer">
+            <div class="pageButtons">
+                <div class="pageNumberContainer">
+                    <img src="assets/images/pageStart.png">
+                </div>
+
+                <div class="pageNumberContainer">
+                    <img src="assets/images/pageEnd.png">
+                </div>
+            </div>
+        </div>
+
     </div>
 </body>
 
