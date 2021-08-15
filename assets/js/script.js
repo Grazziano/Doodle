337ab452ee8f93bb0a6c4c7ea8e0124b1249aaa5
Grazziano Fagundes <grazzianofagundes@gmail.com>
2021-08-14 23:26:46 -0300

    Updating the clicks value via ajax

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,4 +18,13 @@ $(document).ready(function() {
 
 function increaseLinkClicks(linkId, url) {
 
+    $.post("ajax/updateLinkCount.php", { linkId: linkId })
+        .done(function(result) {
+            if (result != "") {
+                alert(result);
+                return;
+            }
+            window.location.href = url;
+        });
+
 }
